1714784beb5621fdc77b66882f31f8a52f7541db
emac62 <borderlineboomer@gmail.com>
2022-03-04 14:31:30 -0500

    live ad units

--- lib/widgets/ad_helper.dart
+++ lib/widgets/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-1618980018345182/2936625698';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-1618980018345182/8688162745';
     } else {
       throw UnsupportedError("Unsupported platform");
     }
@@ -13,9 +13,9 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712";
+      return "ca-app-pub-1618980018345182/7099748907";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4411468910";
+      return "ca-app-pub-1618980018345182/4585266201";
     } else {
       throw UnsupportedError("Unsupported platform");
     }
